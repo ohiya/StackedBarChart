b795bed6d1e88e1c29af32a4aa7f6d932b5e1a0e
Chromolus <schoda@me.com>
2015-03-06 13:56:46 +0100

    Initial Commit

---
+++ StackedBarChart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackedBarChart
+//
+//  Created by Michael Schoder on 06.03.15.
+//  Copyright (c) 2015 Chromo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
